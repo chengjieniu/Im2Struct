1c9f1c0b1cb64f7d7e637186cd223b17d9bbb2f6
chengjieniu <nchengjie@gmail.com>
2018-03-28 23:33:08 +0800

    Update showGenshapes.m

--- showGenshapes.m
+++ showGenshapes.m
@@ -1,19 +1,14 @@
-%将hierarchy进行绘制
 function showGenshapes(genshapes)
 
 for ii =1:length(genshapes)
-%for ii =1:1
     recover_boxes = genshapes{ii}.boxes;
- %   recover_boxes=imdb.images.labels(1861).boxes;
     figure(ii+101);
     axis equal;
     axis([-0.7 0.7 -0.7 0.7 -0.7 0.7]);
-  %axis([-1 1 -1 1 -1 1]);
- % set(gca,'ydir','reverse','xaxislocation','top');
  
- xlabel ( '--X axis--' )
- ylabel ( '--Y axis--' )
- zlabel ( '--Z axis--' )
+     xlabel ( '--X axis--' )
+     ylabel ( '--Y axis--' )
+     zlabel ( '--Z axis--' )
     hold on;
     
     for jj = 1:size(recover_boxes,2)
