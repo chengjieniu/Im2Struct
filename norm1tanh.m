c2977d71ef5c82d76988ffaf0d0babdee5ee6967
chengjieniu <nchengjie@gmail.com>
2018-03-28 23:32:07 +0800

    Add files via upload

---
+++ norm1tanh.m
@@ -0,0 +1,6 @@
+function out = norm1tanh(x)
+    %temp = tanh(x);
+    %out = temp./norm(temp);
+%     out = tanh(x); % don't divide by norm
+    out = tanh(x);
+end
---
+++ norm1tanh_prime.m
@@ -0,0 +1,5 @@
+function out = norm1tanh_prime(a)
+    % x = tanh(x)./norm(tanh(x))
+%     out = (1-a.^2);
+    out = (1-a.^2);
+end
